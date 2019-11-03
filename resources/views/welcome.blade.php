5e731e0471acf48f633f9da5580b473b78c558f1
DanPendragom <carlos.coutinho5@etec.sp.gov.br>
2019-11-03 01:14:40 +0000

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MyDiary</title>
+    <link rel="stylesheet" href="../css/global.css" type="text/css" />
+    <style type="text/css">
+        *{
+            font-family: Arial, Helvetica, Sans-serif;
+            margin:0;
+            padding:0;
+            box-sizing: border-box;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
